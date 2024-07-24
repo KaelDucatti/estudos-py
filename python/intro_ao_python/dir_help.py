046c96c76e8123b4e4dd677b23a28d20dd211208
Kael Ducatti <contato.mikaelducatti@gmail.com>
2024-07-24 20:50:41 -0300

    Ch2: progressing...

--- python/intro_ao_python/dir_help.py
+++ python/intro_ao_python/dir_help.py
@@ -1,1 +1,19 @@
-    
+def my_sum(x, y):
+    """
+    Recebe 2 valores e tenta somá-los
+
+    Args: 
+        x (int, float): primeiro valor
+        y (int, float): segundo valor
+
+    Return:
+        Retorna a soma dos valores em int ou float
+    """
+    return x + y
+
+
+# help
+help(my_sum)
+
+# dir 
+print(dir(my_sum))
---
+++ python/intro_ao_python/recebendo_dados.py
@@ -0,0 +1,12 @@
+"""
+    Recebendo dados do usuário
+"""
+
+# Entrada de dados
+print("Qual o seu nome? ", end="")
+nome = input()
+
+# Saída de dados
+print(f"Bem-vindo(a) {nome}")
+
+
