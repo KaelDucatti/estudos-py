8052e851d6a05c7dd11b623eeac03fee87d77d54
Kael Ducatti <contato.mikaelducatti@gmail.com>
2024-07-31 20:20:20 -0300

    Ch3: progressing...

---
+++ python/03_estruturas_logicas_condicionais/estruturas_logicas.py
@@ -0,0 +1,15 @@
+"""
+Estruturas lógicas: and(e), or(ou), not(não) e is(é)
+
+Operadores unários: not, is
+Operadores binários: and, or
+"""
+
+ativo = True
+logado = True
+
+if ativo and logado:
+    print("Bem vindo(a)")
+else:
+    print("Você precisa ativar sua conta. Por favor cheque seu e-mail")
+
