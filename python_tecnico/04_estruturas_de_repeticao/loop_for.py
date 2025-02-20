70d1847dbac9f75e4158d58b9f095bb2b6cd4cdb
Kael Ducatti <contato.mikaelducatti@gmail.com>
2025-02-20 13:50:21 -0300

    várias alterações

--- python_tecnico/04_estruturas_de_repeticao/loop_for.py
+++
@@ -1,7 +0,0 @@
-quantidade = int(input('Quantas vezes esse loop deve rodar?'))
-soma = 0
-
-for n in range(1, quantidade + 1):
-    num = int(input(    'Informe o {n}/{quantidade} valor: '))
-    soma = soma + num
-print(f'A soma é {soma}')
